c5a8844c8f10c85abb4387f02f29a2deeae94a15
mood7dev <mood7dev@gmail.com>
2025-05-07 12:58:51 +0900

    [feat] 새로 작성

---
+++ js/lib.js
@@ -0,0 +1,30 @@
+const ticketInfoJsonArr = [
+  {
+    title: "팬텀 10주년 기념 공연",
+    img: "https://a.jpg",
+    url: "https://a.html",
+    rank: 1,
+    place: "대구",
+    day: "05/07",
+  },
+  {
+    title: "뮤지컬 <메디슨 카운티의 다리>",
+    img: "https://b.jpg",
+    url: "https://b.html",
+    rank: 2,
+    place: "서울",
+    day: "05/09",
+  },
+  {
+    title: "뮤지컬 <라이카>",
+    img: "https://c.jpg",
+    url: "https://c.html",
+    rank: 3,
+    place: "제주",
+    day: "05/04",
+  },
+];
+
+console.log(ticketInfoJsonArr[0].title); // "팬텀 10주년 기념 공연"
+console.log(ticketInfoJsonArr[1].title); // "뮤지컬 <메디슨 카운티의 다리>"
+console.log(ticketInfoJsonArr[2].title); // "뮤지컬 <라이카>"

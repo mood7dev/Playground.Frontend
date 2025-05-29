ecd125cb5c266a05aedb313425d7317151e9f0ec
mood7dev <mood7dev@gmail.com>
2025-05-29 13:19:41 +0900

    day2 공부

--- day1/js/btn.js
+++ day1/js/btn.js
@@ -34,3 +34,41 @@ toggleButton.addEventListener("click", () => {
     toggleButton.style.backgroundColor = "#1e90ff";
   }
 });
+
+/* ▼단순 스탑워치 ▼ */
+/*
+// ("")은 ('') 입니다.
+const $screen = document.querySelector("#screen");
+const $toggleButton = document.querySelector("#button");
+let timeInterval;
+let stopWatchOn = false;
+let seconds = 0;
+
+console.log("screen:", $screen);
+console.log("toggleButton:", $toggleButton);
+
+if ($screen && $toggleButton) {
+  $toggleButton.addEventListener("click", () => {
+    stopWatchOn = !stopWatchOn;
+    console.log("stopWatchOn: ", stopWatchOn);
+    $toggleButton.innerText = stopWatchOn ? "■" : "▶";
+    $toggleButton.style.backgroundColor = stopWatchOn ? "#ffb7c5" : "#1e90ff";
+    $toggleButton.classList.toggle("stop-coler");
+    if (stopWatchOn) {
+      //첫번째 인자: 콜백함수
+      //두번째 인자: Number - ms
+      timeInterval = setInterval(() => {
+        seconds++;
+        const mm = String(Math.floor(seconds / 6000) % 60).padStart(2, "0");
+        const ss = String(Math.floor(seconds / 100) % 60).padStart(2, "0");
+        const cs = String(Math.floor(seconds % 100)).padStart(2, "0");
+        $screen.innerText = `${mm}:${ss}:${cs}`;
+      }, 10);
+    } else {
+      clearInterval(timeInterval);
+      seconds = 0;
+      $screen.innerText = "00:00:00";
+    }
+  });
+}
+*/
